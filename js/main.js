01dd36f532441ce624f83fd9c4e45a223f7af3b9
OussamaHaddi <oussama.turboxxx1@gmail.com>
2022-10-29 18:01:28 +0100

    add js function qui ajoute un produit sur cart

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 
 
+// nab bar
+
 let navBarIcon = document.querySelector("#nav_bar_icon");
 let navList = document.querySelector("#nav_bar_list");
-let product = document.querySelectorAll(".p1");
 
 navBarIcon.addEventListener("click", _ => {
     navList.classList.toggle("show");
@@ -12,6 +13,72 @@ navBarIcon.addEventListener("click", _ => {
         navBarIcon.setAttribute("src", "./images/icons/icon-close.svg");
     }
 })
+
+
+// change menu
+
+let selectBox = document.querySelector("select");
+let allMenu = document.querySelectorAll(".menu_items");
+let sushi = document.getElementById("sushi");
+let ramen = document.getElementById("ramen");
+let miso = document.getElementById("miso");
+
+let arr = [sushi,ramen,miso]
+
+sushi.style.display = "flex"
+
+
+arr.forEach(el => {
+    selectBox.addEventListener("change" , e => {
+        let selectedElement = document.getElementById(e.target.value);
+        (el == selectedElement) ? selectedElement.style.display = "flex" : el.style.display = "none";
+    });
+})
+
+
+
+// add element to shop
+
+let newObj = {
+    img: "",
+    name: "",
+    quantite: "",
+    price: "",
+};
+
+    // inside shop icon
+let shop = document.querySelector("#shop_items");
+let addItem = document.querySelector("#item_buy");
+    // menu
+let plus = document.querySelectorAll("#plus");
+let minus = document.querySelectorAll("#minus");
+let quantite = parseInt(document.getElementById("quantite"));
+let price = document.getElementById("price");
+let orderNow = document.querySelectorAll(".order_btn");  // order now
+    // all items
+let allItem = document.querySelectorAll("#sushi");
+
+for (let i = 0; i < allItem.length; i++) {
+    orderNow.forEach(el => {
+        el.addEventListener("click", e => {
+            console.log(e.target)
+        })
+    });
+    plus.forEach(ele => {
+        ele.addEventListener("click", e => {
+            console.log(e.target)
+        })
+    });
+    minus.forEach(ele => {
+        ele.addEventListener("click", e => {
+            console.log(quantite.value)
+            
+        })
+    });
+}
+
+
+
 
 
 
